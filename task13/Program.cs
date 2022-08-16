219ceba1eb457a4d8cdac2405948f80bfe7aaba1
Olga <o.o.kiseleva@yandex.ru>
2022-08-16 15:01:04 +0300

    added task13

---
+++ task13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найти третью цифру числа или сообщить, что её нет 
+Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Дано число: {Number}");
+
+if (Number >= 1000)
+{
+    while (Number >= 1000)
+    {
+        Number = Number/10;
+    }
+    int digit3 = Number%10;
+    Console.WriteLine($"Третья цифра числа равна {digit3}");
+}
+else if (Number > 99 && Number < 1000)
+{
+    Console.WriteLine($"Третья цифра числа равна {Number%10}");
+}
+else
+{
+    Console.WriteLine("У числа нет третьей цифры!");
+}
