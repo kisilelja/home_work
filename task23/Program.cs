e00d2c2ced80175251083b72b6a2f0491ba3a5ab
Olga <o.o.kiseleva@yandex.ru>
2022-08-16 17:00:35 +0300

    added task23

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+//и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите  число");
+int N = int.Parse(Console.ReadLine());
+int digit = 1;
+while (digit <= N)
+{
+    Console.Write(digit*digit*digit);
+    Console.Write(", ");
+    digit = digit + 1;
+}
