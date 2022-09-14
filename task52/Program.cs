9a961a9c0c786149531990069765346eb6b457bb
Olga <o.o.kiseleva@yandex.ru>
2022-09-14 20:30:42 +0300

    added task52

---
+++ task52/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+
+Random rand = new Random();
+int[,] arr = new int[rand.Next(1, 10), rand.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = rand.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Среднее арифметическое элементов в каждом столбце равно ");
+
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0):f2} ");
+}
